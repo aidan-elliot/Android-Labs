dbc28333791a48c8e6e4e778a7425279e5826aff
Aidan Elliot <aidanelliot@icloud.com>
2023-09-25 12:50:16 -0400

    Working on Snackbar message and undo button

--- app/src/main/java/com/cst3104/id41080471/MainActivity.kt
+++ app/src/main/java/com/cst3104/id41080471/MainActivity.kt
@@ -2,8 +2,12 @@ package com.cst3104.id41080471
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.Switch
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +16,22 @@ class MainActivity : AppCompatActivity() {
 
         val toastmessage = findViewById<Button>(R.id.button3)
         toastmessage.setOnClickListener {
-            Toast.makeText(this@MainActivity, R.string.toast_message, Toast.LENGTH_LONG).show()
+            Toast.makeText(this@MainActivity, R.string.toast_message, Toast.LENGTH_LONG)
+                .show()
         }
+        val switch = findViewById<Switch>(R.id.switch1)
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            val message = "${R.string.switchState} ${if (isChecked) R.string.On else R.string.Off}"
+            showSnackbar(message)
+        }
+    }
+    private fun showSnackbar(message: String) {
+        val snackbar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT)
+        snackbar.setAction(R.string.undoString, View.OnClickListener {
+
+        })
+        snackbar.show()
     }
 
 }
