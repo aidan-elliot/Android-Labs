7ee4813484f582ebaf74aaeef08d0f8438755576
Aidan Elliot <aidanelliot@icloud.com>
2023-10-02 15:02:20 -0400

    Finished lab 3.

--- app/src/main/java/com/cst3104/id41080471/MainActivity.kt
+++ app/src/main/java/com/cst3104/id41080471/MainActivity.kt
@@ -2,8 +2,11 @@ package com.cst3104.id41080471
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.RotateAnimation
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Switch
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
@@ -12,29 +15,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toastmessage = findViewById<Button>(R.id.button2)
-        toastmessage.setOnClickListener {
-            Toast.makeText(this@MainActivity, R.string.toast_message, Toast.LENGTH_LONG)
-                .show()
-        }
-        val switch = findViewById<Switch>(R.id.switch1)
+        val flag = findViewById<ImageView>(R.id.flag)
+        val switch = findViewById<Switch>(R.id.spin_switch)
+
+
+
         switch.setOnCheckedChangeListener { _, isChecked ->
-            val switchState = getString(R.string.switchState)
-            val switchstatus = if (isChecked) getString(R.string.On) else getString(R.string.Off)
-            val message = "$switchState $switchstatus"
-            showSnackbar(message, switch)
-        }
-    }
-    private fun showSnackbar(message: String, switch: Switch) {
-        val snackbar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT)
-        snackbar.setAction(R.string.undoString) {
-            if (switch.isChecked){
-                switch.isChecked = false
-            } else{
-                switch.isChecked = true
+            if (isChecked) {
+                val rotate = AnimationUtils.loadAnimation(this, R.anim.rotate_clockwise)
+
+                flag.startAnimation(rotate)
+            }else{
+                flag.clearAnimation()
             }
         }
-        snackbar.show()
     }
-
 }
