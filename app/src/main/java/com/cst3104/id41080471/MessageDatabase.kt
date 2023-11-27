91cfaebbe6b7971962d491ce279354c627557c69
Aidan Elliot <aidanelliot@icloud.com>
2023-11-27 11:41:19 -0500

    Non working state

--- app/src/main/java/com/cst3104/id41080471/MessageDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.cst3104.id41080471
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [MessageData::class], version = 1)
-abstract class MessageDatabase : RoomDatabase() {
-    abstract fun chatMessageDao(): ChatMessageDAO
-}
